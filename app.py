f630e618d8d25c7d74f3b09f225c57dec17a3e52
Shivaay20005 <lava.blaze.5g.pro@gmail.com>
2025-06-06 10:20:50 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
-# Copyright (c) 2025 devgagan : https://github.com/Shivaay20005.  
-# Licensed under the GNU General Public License v3.0.  
+# # Copyright (c) 2025 devgagan : https://github.com/Shivaay20005.  
+# # Licensed under the GNU General Public License v3.0.  
+# # See LICENSE file in the repository root for full license text.
+
+# import os
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def welcome():
+#     # Render the welcome page with animated "Team SPY" text
+#     return render_template("welcome.html")
+
+# if __name__ == "__main__":
+#     # Default to port 5000 if PORT is not set in the environment
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
+
+
+# Copyright (c) 2025 devgagan : https://github.com/Shivaay20005.
+# Licensed under the GNU General Public License v3.0.
 # See LICENSE file in the repository root for full license text.
 
 import os
-from flask import Flask, render_template
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    # Render the welcome page with animated "Team SPY" text
-    return render_template("welcome.html")
+    # Redirect directly to your Telegram profile
+    return redirect("https://t.me/Shivaay20005", code=302)
 
 if __name__ == "__main__":
     # Default to port 5000 if PORT is not set in the environment
